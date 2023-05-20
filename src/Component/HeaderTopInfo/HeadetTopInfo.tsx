2f25e0aa78c3ea92285a9c581aff04204d13a99a
Marko Vasic <perionicavasic2018@gmail.com>
2023-05-20 17:45:04 +0200

    HeadetTopInfo - styles

--- src/Component/HeaderTopInfo/HeadetTopInfo.tsx
+++ src/Component/HeaderTopInfo/HeadetTopInfo.tsx
@@ -1,46 +1,59 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./HeaderTopInfo.css";
+
 import { useTypedSelector } from "../../hooks/useTypedSelector";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+
 import { selectUserName } from "../../redux/user/userSlice";
 import { selectWorkerName, setWorker } from "../../redux/worker/workerSlice";
-import { useAppDispatch } from "../../hooks/useAppDispatch";
+
+import { Button } from "../layout/button/button";
+import { Modal } from "../modal/modal";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faListDots } from "@fortawesome/free-solid-svg-icons";
 
 export default function HeaderTopInfo() {
   const dispatch = useAppDispatch();
   const userName = useTypedSelector(selectUserName);
   const workerName = useTypedSelector(selectWorkerName);
 
+  const [showWorker, setShowWorker] = useState(false);
+
   const navigate = useNavigate();
 
   return (
-    <div className="header-reception">
-      <div className="washer-logo">
-        <h2>WASHER</h2>
+    <>
+      <div className="header-reception">
+        <div className="washer-logo" >
+          <h2>WASHER</h2>
+        </div>
+        <span onClick={() => setShowWorker(!showWorker)}><FontAwesomeIcon icon={faListDots} /></span>
       </div>
-      <div className="user">
-        <p>
-          Korisnik: <span>{userName}</span>
-        </p>
-      </div>
-      <div className="worker">
-        <p>
-          Radnik: <span>{workerName}</span>
-        </p>
-      </div>
-      <div className="logOutButton">
-        <button
-          onClick={() => {
-            dispatch(setWorker({
-              workerName: "",
-              workerId: 0,
-              workerLogIn: false,
-            }));
-            navigate("/workerlogin");
-          }}
-        >
-          Odjava radnika
-        </button>
-      </div>
-    </div>
+
+      <Modal open={showWorker} close={() => setShowWorker(!showWorker)}>
+          <div className="info-mobile">
+            <div className="animation-0">
+              <p>Korisnik</p>
+              <span>{userName}</span>
+            </div>
+            <div className="animation-1">
+              <p>Radnik</p>
+              <span>{workerName}</span>
+            </div>
+            <Button
+              default
+              onClickFunction={() => {
+                dispatch(setWorker({
+                  workerName: "",
+                  workerId: 0,
+                  workerLogIn: false,
+                }));
+                navigate("/workerlogin");
+              }}
+              title="Odjava radnika" />
+          </div>
+      </Modal>
+    </>
   );
 }
